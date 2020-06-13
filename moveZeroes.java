f759d409b024230601bfc5a2919faea9d90ad10d
zhengyitong <zhengyitong0928@126.com>
2020-06-13 08:02:38 +0800

    week01作业-2

--- moveZeroes.java
+++ moveZeroes.java
@@ -1,10 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int zeroNums = 0;
-        for (int i =0;i<nums.length();i++) {
-            
+        int tempNum = 0;
+        
+        int j = 0;
+       
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] != 0 ) {
+                nums[j++] = nums[i];
+            }
         }
 
+        for (int i=j;i<nums.length;i++) {
+            nums[i] = 0;
+
+        }
 
     }
+
 }
